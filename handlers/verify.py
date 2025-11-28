ed20e4472774f1dc749eeb1948d582608859e3a3
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 13:34:06 +0530

    Update verify.py

--- handlers/verify.py
+++ handlers/verify.py
@@ -1,22 +1,21 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from utils.check_member import check_all_membership
 from utils.buttons import verify_buttons, start_menu
 
 router = Router()
 
-
-@router.callback_query(lambda c: c.data == "check_verify")
+@router.callback_query(F.data == "check_verify")
 async def verify_callback(callback: types.CallbackQuery, bot):
     user_id = callback.from_user.id
 
     ok = await check_all_membership(bot, user_id)
     if not ok:
         return await callback.message.edit_text(
-            "❌ <b>You still haven’t joined all channels.</b>\nPlease join and tap again.",
+            "❌ You still haven’t joined all channels.\nJoin all & tap again.",
             reply_markup=verify_buttons()
         )
 
     await callback.message.edit_text(
-        "✅ <b>Verification Complete!</b>\nChoose a tool below:",
+        "✅ Verification complete!\nChoose a tool below:",
         reply_markup=start_menu()
     )

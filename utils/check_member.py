d167f1db35d5fb80c8ad89e8de778e05021bd082
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 14:34:59 +0530

    Update check_member.py

--- utils/check_member.py
+++ utils/check_member.py
@@ -1,24 +1,12 @@
 from aiogram import Bot
-from config import REQUIRED_CHANNELS, REQUIRED_GROUP
+from config import REQUIRED_CHANNELS
 
-
-async def check_all_membership(bot: Bot, user_id: int) -> bool:
-    # Check all channels
-    for channel in REQUIRED_CHANNELS:
+async def check_all_membership(bot: Bot, user_id: int):
+    for ch in REQUIRED_CHANNELS:
         try:
-            m = await bot.get_chat_member(channel, user_id)
-            if m.status in ("left", "kicked"):
+            mem = await bot.get_chat_member(ch, user_id)
+            if mem.status not in ["member", "administrator", "creator"]:
                 return False
         except:
             return False
-
-    # Check required group
-    try:
-        group = REQUIRED_GROUP.replace("https://t.me/", "")
-        m = await bot.get_chat_member(group, user_id)
-        if m.status in ("left", "kicked"):
-            return False
-    except:
-        return False
-
     return True

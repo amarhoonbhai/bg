56399483a096bd847acc33634ade61d9dbdc530b
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 14:56:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from handlers import start, menu, remove_bg, cutout, stats, broadcast
 
+
 async def main():
-    bot = Bot(BOT_TOKEN, parse_mode="HTML")
+    # Init bot
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
 
+    # global var for mode
     bot.user_mode = None
 
+    # include routers
     dp.include_router(start.router)
     dp.include_router(menu.router)
     dp.include_router(remove_bg.router)
@@ -16,8 +21,10 @@ async def main():
     dp.include_router(stats.router)
     dp.include_router(broadcast.router)
 
-    print("🔥 Bot is running...")
+    print("🔥 BOT STARTED SUCCESSFULLY — READY TO USE")
+
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())

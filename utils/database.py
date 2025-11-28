c22f5a920ab2512710029a6f3cc4d5d3fa90e9ed
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 12:57:01 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -6,9 +6,23 @@ def init_db():
     conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
 
+    # Users DB
     cur.execute("""
         CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY
+            user_id INTEGER PRIMARY KEY,
+            joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+    # Stats Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS stats (
+            id INTEGER PRIMARY KEY,
+            bg INTEGER DEFAULT 0,
+            enhance INTEGER DEFAULT 0,
+            dp INTEGER DEFAULT 0,
+            face INTEGER DEFAULT 0,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     """)
 
@@ -19,7 +33,6 @@ def init_db():
 def add_user(user_id):
     conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
-
     cur.execute("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
     conn.commit()
     conn.close()
@@ -28,9 +41,14 @@ def add_user(user_id):
 def get_all_users():
     conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
-
     cur.execute("SELECT user_id FROM users")
     users = [u[0] for u in cur.fetchall()]
-
     conn.close()
     return users
+
+
+# ---- STATS FUNCTIONS ---- #
+
+def increment_stat(column: str):
+    """column must be: bg / enhance / dp / face"""
+    conn = sqli

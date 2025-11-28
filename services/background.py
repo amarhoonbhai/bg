aa7d9a63ed215dd08bc773519b70681e8b2fa64f
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 15:02:28 +0530

    Update background.py

--- services/background.py
+++ services/background.py
@@ -1,8 +1,16 @@
-from io import BytesIO
 from rembg import remove
+from io import BytesIO
 
-def remove_background(img_bytes):
-    output = remove(img_bytes)
-    buf = BytesIO(output)
-    buf.seek(0)
-    return buf
+
+def remove_background(image_bytes: bytes) -> BytesIO:
+    """
+    Removes background from an image using rembg.
+    """
+    try:
+        output = remove(image_bytes)
+        buf = BytesIO(output)
+        buf.seek(0)
+        return buf
+    except Exception as e:
+        print("Error in BG removal:", e)
+        return None

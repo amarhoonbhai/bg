9d75c87781602f1549a48ed1ba5d17cb99b2c864
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 13:07:17 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,9 @@
 from aiogram import Router, types
-from utils.database import add_user, init_db
+from utils.database import add_user
 from utils.check_member import check_all_membership
 from utils.buttons import start_menu, verify_buttons
 
 router = Router()
-
-init_db()
 
 
 @router.message(commands={"start"})
@@ -16,12 +14,11 @@ async def start_cmd(message: types.Message, bot):
     ok = await check_all_membership(bot, user_id)
     if not ok:
         return await message.answer(
-            "🔐 <b>To use the bot, please join all required channels.</b>",
+            "🔐 <b>To use the bot, please join all required channels below:</b>",
             reply_markup=verify_buttons()
         )
 
     await message.answer(
-        "<b>✨ Welcome to Instant BG Remover Bot!</b>\nChoose a tool below:",
+        "✨ <b>Welcome to Instant BG Remover Bot!</b>\nChoose a tool below:",
         reply_markup=start_menu()
     )
-

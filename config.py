7e9fc54a66e45b5b7f430c11ad98bc0fa1649a96
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 13:01:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,15 @@
-import os
+BOT_TOKEN = "PUT-YOUR-BOT-TOKEN-HERE"
 
-# BOT TOKEN (insert your token here)
-BOT_TOKEN = "8580026385:AAFMuH2ceUwZ99HxCVTe2DCoQJUTRSeFcmg"
-
-# Admin ID for broadcast
 ADMIN_ID = -5365568568
 
-# Required channels
+# Required Channels
 REQUIRED_CHANNELS = [
     "@PhiloBots",
     "@TheTrafficZone",
     "@ClaimBack"
 ]
 
-# Required GC (private/public)
+# Required Group Invite Link or Username
 REQUIRED_GROUP = "https://t.me/+X83tuZcK0FkwZWY1"
 
-# Local database file
 DB_PATH = "users.db"

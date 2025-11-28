5392480c0f1f1de8ab8cd6c8e6e7b7a3a8a67f46
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 13:03:06 +0530

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,11 +1,9 @@
 from aiogram import types
-from aiogram.filters import CommandObject
 from config import ADMIN_ID
 
-
-def admin_only(handler):
+def admin_only(func):
     async def wrapper(message: types.Message, *args, **kwargs):
         if message.from_user.id != ADMIN_ID:
             return await message.answer("❌ You are not allowed to use this command.")
-        return await handler(message, *args, **kwargs)
+        return await func(message, *args, **kwargs)
     return wrapper

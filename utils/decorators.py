94fda519427de8a01652c7588a152337f04770e3
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 15:01:21 +0530

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,9 +1,10 @@
 from aiogram.types import Message
 from config import ADMIN_IDS
 
+
 def admin_only(func):
     async def wrapper(message: Message, *args, **kwargs):
         if message.from_user.id not in ADMIN_IDS:
-            return await message.answer("❌ You are not allowed to use this command.")
+            return await message.answer("❌ You are not authorized.")
         return await func(message, *args, **kwargs)
     return wrapper

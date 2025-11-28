cacedd8c347b8cabd7aca8c46c028631d76cbc71
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-11-28 14:58:46 +0530

    Update check_member.py

--- utils/check_member.py
+++ utils/check_member.py
@@ -1,12 +1,23 @@
 from aiogram import Bot
-from config import REQUIRED_CHANNELS
+from config import REQUIRED_CHANNELS, REQUIRED_GROUP
+
 
 async def check_all_membership(bot: Bot, user_id: int):
-    for ch in REQUIRED_CHANNELS:
+    # check channels
+    for channel in REQUIRED_CHANNELS:
         try:
-            mem = await bot.get_chat_member(ch, user_id)
-            if mem.status not in ["member", "administrator", "creator"]:
+            member = await bot.get_chat_member(channel, user_id)
+            if member.status not in ["member", "administrator", "creator"]:
                 return False
         except:
             return False
+
+    # check group
+    try:
+        member = await bot.get_chat_member(REQUIRED_GROUP, user_id)
+        if member.status not in ["member", "administrator", "creator"]:
+            return False
+    except:
+        return False
+
     return True
